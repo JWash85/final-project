e835c441a2515ce9516232fa88c578f88f1a282b
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-08-19 15:59:50 -0400

    added player and movement from tutorial

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     scale: {
-        mode: Phaser.Scale.FIT,
+       // mode: Phaser.Scale.FIT,
         width: 800,
         height: 600,
     },
