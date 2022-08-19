61dadce19246c21f9bfa7750cb5459722e5ff318
Jarmarr Washington <jarmarrwashington@Jarmarrs-iMac.local>
2022-08-18 20:46:16 -0400

    added ground from tutorial

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -5,14 +5,15 @@ import bombImg from '../assets/bomb.png'
 import groundImg from '../assets/platform.png'
 import starImg from '../assets/star.png'
 import spriteImg from '../assets/dude.png'
+
 class GameScene extends Scene
 {
    
 
     preload ()
     {
-        this.load.image('sky', skyImg)
-        this.load.image('ground', groundImg );
+       this.load.image('sky', skyImg)
+       this.load.image('ground', groundImg );
        this.load.image('star', starImg );
        this.load.image('bomb', bombImg);
        this.load.spritesheet('dude', spriteImg,
@@ -22,10 +23,18 @@ class GameScene extends Scene
       
     create ()
     {
-        
-        this.add.image(400, 300, 'sky')
-        this.add.image(400, 150, 'bomb')
-      
+        const sky = this.add.image(0, 0, 'sky')
+        sky.setOrigin(0, 0)
+
+        this.createPlatforms()
+    }
+
+    createPlatforms() {
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
     }
 }
 
